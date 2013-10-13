6c7f3266832c4718414c38d743150ad55b6417ca
werehyena <scottemorton@comcast.net>
2013-10-13 16:57:23 -0400

    added code for real

---
+++ Zoidberg2012.cpp
@@ -0,0 +1,46 @@
+#include "WPILib.h"
+
+
+class Zoidberg2012 : public IterativeRobot
+{
+	
+public:
+	Zoidberg2012(void)	{
+		
+	}
+	
+	/********************************** Init Routines *************************************/
+
+	void RobotInit(void) {
+	}
+	
+	void DisabledInit(void) {
+
+	}
+
+	void AutonomousInit(void) {
+
+	}
+
+	void TeleopInit(void) {
+
+	}
+
+	/********************************** Periodic Routines *************************************/
+	
+	void DisabledPeriodic(void)  {
+
+	}
+
+	void AutonomousPeriodic(void) {
+		
+	}
+
+	
+	void TeleopPeriodic(void) {
+		
+	} 
+			
+};
+
+START_ROBOT_CLASS(Zoidberg2012);
